689738ce857930a3c083967a8497d229f46e938d
Bryan Moreno <bfourbryan@gmail.com>
2021-01-02 14:57:10 -0800

    Addition of remaining data points to generate markdown file, js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,29 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ##Description 
+  ${data.description}
+
+  ##Installation Guide
+  ${data.installation-instructions}
+
+  ##Usage Guidelines
+  ${data.usage}
+
+  ##How to Contribute
+  ${data.contributing}
+
+  ##Testing
+  ${data.tests}
+
+  ##License
+  ${data.ChosenLicense}
+
+  ##Questions
+  //Comment on ways to contact for more information or possible collaborations
+  ${data.github}
+  ${data.email}
+
 // Duplicate line 3 of code to get the other prompts to display here. use appropriate #'s to give appropriate headings 
 `;
 }
